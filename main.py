c8f93b671fc9993523a4bf7ff35436785cd10546
jakubek0708 <jakubek0708@gmail.com>
2021-03-21 21:27:25 +0100

    juan

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import discord
+from discord.ext import *
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+client = commands.Bot(command_prefix = '$')
+@client.event #when bot is turned on
+async def on_ready():
+    print("akadembot gotowy B)")
+    await client.change_presence(activity=discord.Game(name="komendy: $komendy"))
+client.run(TOKEN)
