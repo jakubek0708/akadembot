81e7fb7c44b8c3d5cc7169486f1ee1cd898c1006
jakubek0708 <jakubek0708@gmail.com>
2021-03-23 18:38:01 +0100

    nice

--- bot.py
+++ bot.py
@@ -19,9 +19,9 @@ async def on_ready():
 @client.event #licznie członków po dołączeniu
 async def on_member_join(member):
     channel = client.get_channel(820634014662131773)
-    with open("members.txt", 'r') as f:
+    with open("/opt/akadembot/members.txt", 'r') as f:
         number = int(f.read()) + 1
-    with open("members.txt", 'w') as f:
+    with open("/opt/akadembot/members.txt", 'w') as f:
         f.truncate()
         f.write(str(number))
     await channel.send(f"{member} jest {number} użytkownikiem")
@@ -37,7 +37,7 @@ async def komendy(ctx):
 async def members(ctx, *, message = None):
     role = discord.utils.get(ctx.guild.roles, name="admini")
     if message == None:
-        with open("members.txt", 'r') as f:
+        with open("/opt/akadembot/members.txt", 'r') as f:
             number = f.read()
             string = f"Obecnie na serwerze jest: {number} osób"
             await ctx.send(string)
@@ -48,7 +48,7 @@ async def members(ctx, *, message = None):
             except:
                 await ctx.send("Musisz wpisać liczbę")
             number = str(number)
-            with open("members.txt", 'w') as f:
+            with open("/opt/akadembot/members.txt", 'w') as f:
                 await ctx.send(f"Zmieniam liczbę użytkowników na: {number}")
                 f.truncate()
                 f.write(number)
