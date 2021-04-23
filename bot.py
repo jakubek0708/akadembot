9efbf3b137b1bbe5e142e517f64ba32a756b889c
jakubek0708 <jakubek0708@gmail.com>
2021-04-23 20:05:08 +0200

    added memifejioafjoia

--- bot.py
+++ bot.py
@@ -22,13 +22,14 @@ async def on_ready():
 
 @client.event  # licznie członków po dołączeniu
 async def on_member_join(member):
+    global member_three_number
     channel = client.get_channel(820634014662131773)
-    with open("/opt/akadembot/members.txt", 'r') as f:
+    with open("members.txt", 'r') as f:
         number = int(f.read()) + 1
-    with open("/opt/akadembot/members.txt", 'w') as f:
+    with open("members.txt", 'w') as f:
         f.truncate()
         f.write(str(number))
-    await channel.send(f"({member_three_number}){member} jest {number} użytkownikiem")
+    await channel.send(f"({member_three_number}) {member} jest {number} użytkownikiem")
     if member_three_number < 3:
         member_three_number += 1
     else:
@@ -48,7 +49,7 @@ async def komendy(ctx):
 async def members(ctx, *, message=None):
     role = discord.utils.get(ctx.guild.roles, name="admini")
     if message == None:
-        with open("/opt/akadembot/members.txt", 'r') as f:
+        with open("members.txt", 'r') as f:
             number = f.read()
             string = f"Obecnie na serwerze jest: {number} osób"
             await ctx.send(string)
@@ -59,7 +60,7 @@ async def members(ctx, *, message=None):
             except:
                 await ctx.send("Musisz wpisać liczbę")
             number = str(number)
-            with open("/opt/akadembot/members.txt", 'w') as f:
+            with open("members.txt", 'w') as f:
                 await ctx.send(f"Zmieniam liczbę użytkowników na: {number}")
                 f.truncate()
                 f.write(number)
